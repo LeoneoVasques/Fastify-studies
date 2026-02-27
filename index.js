95a83aa87513b0aa9cb27c9270340dbfe485a979
Leoneo Vasques <leoneo.vasques@gmail.com>
2026-02-27 17:35:41 -0300

    🔒️ security: Add bcrypt.

--- index.js
+++ index.js
@@ -2,34 +2,53 @@ const fastify = require("fastify");
 
 const app = fastify();
 const users = [
-    { id: 1, name: "Alice", age: 30 },
-    { id: 2, name: "Bob", age: 25 },
-    { id: 3, name: "Charlie", age: 35 },
+  { id: 1, username: "alice", age: 30, password: "123" },
+  { id: 2, username: "bob", age: 25, password: "123" },
+  { id: 3, username: "pedro", age: 35, password: "123" },
 ];
 
-app.post("/users", function handler(request, response) {
+const bcrypt = require("bcrypt");
+
+async function createHash(purePassword) {
+  const saltRounds = 5;
+
+  const hash = await bcrypt.hash(purePassword, saltRounds);
+
+  return hash;
+}
+
+app.post("/users", async function handler(request, response) {
   const data = request.body;
-  if (!data.name || !data.age) {
-    return response.status(400).send({ error: "Name and age are required" });
+  if (!data.username || !data.age || !data.password) {
+    return response
+      .status(400)
+      .send({ error: "Username, age, and password are required" });
   }
-  const newUser = { id: users.length + 1, name: data.name, age: data.age };
+  const newUser = {
+    id: users.length + 1,
+    username: data.username,
+    age: data.age,
+    password: await createHash(data.password),
+  };
   users.push(newUser);
 
   response.status(201).send(newUser);
 });
 
 app.get("/users", function handler(request, response) {
-    const query = request.query;
-    if (query.name) {
-        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(query.name.toLowerCase()));
-        return response.send(filteredUsers);
-    }
-    response.send(users);
+  const query = request.query;
+  if (query.username) {
+    const filteredUsers = users.filter((user) =>
+      user.username.toLowerCase().includes(query.username.toLowerCase()),
+    );
+    return response.send(filteredUsers);
+  }
+  response.send(users);
 });
 
 app.get("/users/:id", function handler(request, response) {
   const id = parseInt(request.params.id);
-  const user = users.find(user => user.id === id);
+  const user = users.find((user) => user.id === id);
   if (!user) {
     return response.status(404).send({ error: "User not found" });
   }
@@ -38,7 +57,7 @@ app.get("/users/:id", function handler(request, response) {
 
 app.patch("/users/:id", function handler(request, response) {
   const id = parseInt(request.params.id);
-  const user = users.find(user => user.id === id);
+  const user = users.find((user) => user.id === id);
   if (!user) {
     return response.status(404).send({ error: "User not found" });
   }
@@ -51,13 +70,13 @@ app.patch("/users/:id", function handler(request, response) {
 
 app.delete("/users/:id", function handler(request, response) {
   const id = parseInt(request.params.id);
-  const userId = users.findIndex(user => user.id === id);
+  const userId = users.findIndex((user) => user.id === id);
 
-    if (userId === -1) {
-      return response.status(404).send({ error: "User not found" });
-    }
-    users.splice(userId, 1);
-    response.send({ message: "User deleted successfully" });
+  if (userId === -1) {
+    return response.status(404).send({ error: "User not found" });
+  }
+  users.splice(userId, 1);
+  response.send({ message: "User deleted successfully" });
 });
 
 // Run the server!
